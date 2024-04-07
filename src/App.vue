f7c206f589cc9c71e9397ba2df2cfc61af304cf1
Franziska Bath <franziska.bath@strato.de>
2024-04-07 22:33:47 +0200

    add posts list view component
    for now with dummy posts data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
+import PostsList from './components/PostsList.vue';
 </script>
 
 <template>
   <header>
-    <h1>Post-Manager</h1>
+    <h2 class="text-2xl font-bold leading-7 text-gray-700 sm:truncate sm:text-3xl sm:tracking-tight">
+      Post Manager
+    </h2>
   </header>
-
   <main>
+    <PostsList />
   </main>
 </template>
 
---
+++ src/components/PostsList.vue
@@ -0,0 +1,28 @@
+<script setup>
+const tableHeaders = ["Title", "Description", "Author"];
+const posts = [{
+  "userId": 2,
+  "id": 42,
+  "title": "dummy title",
+  "body": "dummy body"
+}]
+</script>
+
+<template>
+  <table id="posts-list" class="table-auto border-collapse border border-slate-500 text-sm mt-6">
+    <thead class="bg-gray-700 text-white">
+    <tr>
+      <th v-for="header in tableHeaders" :key='header' class="w-1/3 py-3 font-semibold">
+        {{header}}
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="post in posts" :key='post' class="odd:bg-white even:bg-gray-200">
+      <td class="text-center first-letter:uppercase py-3 px-2">{{post.title}}</td>
+      <td class="text-left first-letter:uppercase py-3 px-2">{{post.body}}</td>
+      <td class="text-center py-3 px-2">User #{{post.userId}}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
